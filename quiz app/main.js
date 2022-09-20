bb19afa4f36f84bf5f9c0d37ebdff0d089cf4d37
amirafatnassi <amirafatnassi88@gmail.com>
2022-09-20 23:41:29 +0100

    create bullets function quiz app

--- quiz app/main.js
+++ quiz app/main.js
@@ -1,3 +1,7 @@
+//select elements
+let countSpan = document.querySelector(".count span");
+let bulletsSpanContainer = document.querySelector(".bullets .spans");
+
 function getQuestions() {
   let myRequest = new XMLHttpRequest();
   myRequest.onreadystatechange = function () {
@@ -11,11 +15,32 @@ function getQuestions() {
     //404: not found
     //200: response ok
     if (this.readyState === 4 && this.status === 200) {
-      let questionObject = JSON.parse(this.responseText);
-      console.log(questionObject);
+      let questionsObject = JSON.parse(this.responseText);
+      let questionsCount = questionsObject.length;
+
+      //create bullets & set questions count
+      createBullets(questionsCount);
     }
   };
   myRequest.open("GET", "questions.json", true);
   myRequest.send();
 }
 getQuestions();
+
+function createBullets(num) {
+  countSpan.innerHTML = num;
+
+  //create spans
+  for (let i = 0; i < num; i++) {
+    //create span
+    let theBullet = document.createElement("span");
+
+    //check if its first span
+    if (i === 0) {
+      theBullet.className = "on";
+    }
+
+    //apend bullets to main bullet container
+    bulletsSpanContainer.appendChild(theBullet);
+  }
+}
