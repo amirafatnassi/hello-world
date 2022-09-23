77e401bc20c32b1df5b737ca8657c84f151c20f8
amirafatnassi <amirafatnassi88@gmail.com>
2022-09-23 09:07:13 +0100

    splash screen

---
+++ memory game/main.js
@@ -0,0 +1,9 @@
+document.querySelector(".control-buttons span").onclick = function () {
+  let yourName = prompt("What's your name ?");
+  if (yourName === null || yourName === "") {
+    document.querySelector(".name span").innerHTML = "Unknown";
+  } else {
+    document.querySelector(".name span").innerHTML = yourName;
+  }
+  document.querySelector(".control-buttons").remove();
+};
