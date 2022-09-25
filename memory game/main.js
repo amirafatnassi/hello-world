aedebd67a5e74fedaa121b2fa5ec01a4314fa75e
amirafatnassi <amirafatnassi88@gmail.com>
2022-09-25 22:12:29 +0100

    shuffle function memory game

--- memory game/main.js
+++ memory game/main.js
@@ -19,9 +19,30 @@ let blocks = Array.from(blocksContainer.children);
 //create range of keys
 //let orderRange = [...Array(blocks.length).keys()];
 let orderRange = Array.from(blocks.length).keys();
-
+shuffle(orderRange);
 
 //add order css propert to game blocks
 blocks.forEach((block, index) => {
   block.style.order = orderRange[index];
 });
+
+//shuffle function
+function shuffle(array) {
+  //settings vars
+  let current = array.length,
+    temp,
+    random;
+  while (current > 0) {
+    //get random number
+    random = Math.floor(Math.random() * current);
+    //decrease length by 1
+    current--;
+    //1- save current element in stash
+    temp = array[current];
+    //2- current element = random element
+    array[current] = array[random];
+    //3- random element = get element from stash
+    array[random] = temp;
+  }
+  return array;
+}
